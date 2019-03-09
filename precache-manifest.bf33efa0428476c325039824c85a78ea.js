7b8c53289fa6ae38f2915c4d1523c6f98596a13b
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-03-09 00:59:45 -0800

    Updates

---
+++ precache-manifest.bf33efa0428476c325039824c85a78ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e28cc3b1a45be8e53bdf",
+    "url": "/click-game/static/js/runtime~main.a99f9c06.js"
+  },
+  {
+    "revision": "e1f6799f15d4f4d70b4e",
+    "url": "/click-game/static/js/main.13996e37.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/click-game/static/js/2.f60c3f33.chunk.js"
+  },
+  {
+    "revision": "e1f6799f15d4f4d70b4e",
+    "url": "/click-game/static/css/main.f2d32bf1.chunk.css"
+  },
+  {
+    "revision": "8521d2f204d676b1e1d42b5ffecc5d9d",
+    "url": "/click-game/index.html"
+  }
+];
