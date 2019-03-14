94e9744509cdba12f0bbef281b970c879092f55b
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-03-13 19:25:26 -0700

    updated URL in navbar component

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -4,7 +4,7 @@ import "./navbar.css";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-info">
-      <a className="navbarText navbar-brand text-center" href="/">
+      <a className="navbarText navbar-brand text-center" href="https://vaheminasyan2.github.io/click-game">
         Clicky Game
       </a>
     </nav>
